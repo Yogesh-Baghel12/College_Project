7fb9357d157935f6ee69d5b7ccb23ce6afb1a790
yogesh baghel <139314033+Yogesh-Baghel12@users.noreply.github.com>
2024-01-06 14:37:20 +0530

    Changes in CSS

--- A2SL/views.py
+++ A2SL/views.py
@@ -125,7 +125,6 @@ def login_view(request):
 	if request.method == 'POST':
 		form = AuthenticationForm(data=request.POST)
 		if form.is_valid():
-			#log in user
 			user = form.get_user()
 			login(request,user)
 			if 'next' in request.POST:
